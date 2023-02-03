8aa695883ac10dfd15155b4f97670e03d6c1e0a8
Zesky665 <zharkoc@gmail.com>
2023-02-03 18:49:55 +0100

    update dockerfile

--- Week_2/solution/setup.sh
+++ Week_2/solution/setup.sh
@@ -1,25 +1,14 @@
 # step 1
-mylog 1 "Update"
 apt-get update -y
 
 # step 2
-mylog 3 "Install python"
 apt-get install -y python3
 
 # step 3
-mylog 4 "Install pip"
 apt-get install -y pip
 
-# step 4
-mylog 5 "Install prefect"
-pip install prefect
-
 # step 5
-mylog 6 "Install s3fs"
 pip install s3fs
-
-# step 6
-pip install wget
 
 # step 7
 pip install numpy
